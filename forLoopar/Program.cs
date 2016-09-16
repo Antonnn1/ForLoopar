66cbba8d7f4d57ecb1e7ece045d10bff2e0ff5d9
antonnn1 <antonnn@vgy.se>
2016-09-16 14:20:18 +0200

    Ovn3

--- forLoopar/Program.cs
+++ forLoopar/Program.cs
@@ -13,9 +13,13 @@ namespace forLoopar
             /*
             for (int i = 0; i <= 100; i++) { Console.WriteLine(i); }
             */
-            
+
+            /*
             for (int i = 20; i >= 1; i--){ Console.WriteLine(i); }
-            
+            */
+
+            for(int i = 0; i<=50; i=i + 2) { Console.WriteLine(i); }
+
         }
     }
 }
