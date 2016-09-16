ac9ace3883d3f984c37fa409200a505806f5fa17
antonnn1 <antonnn@vgy.se>
2016-09-16 14:06:22 +0200

    Add project files.

---
+++ forLoopar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
