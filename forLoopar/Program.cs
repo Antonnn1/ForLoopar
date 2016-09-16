8745cf9384a01f985dca380d0b4fd66c74227ddd
antonnn1 <antonnn@vgy.se>
2016-09-16 14:33:34 +0200

    Ovn 2.1

--- forLoopar/Program.cs
+++ forLoopar/Program.cs
@@ -11,14 +11,31 @@ namespace forLoopar
         static void Main(string[] args)
         {
             /*
+            // Ovn 1
             for (int i = 0; i <= 100; i++) { Console.WriteLine(i); }
             */
 
             /*
+            //Ovn 2
             for (int i = 20; i >= 1; i--){ Console.WriteLine(i); }
             */
 
+            /*
+            //Ovn 3
             for(int i = 0; i<=50; i=i + 2) { Console.WriteLine(i); }
+            */
+
+            //Ovn 2.1
+            Console.WriteLine("Mata in start: ");
+            string inmatat;
+            inmatat = Console.ReadLine();
+            int start = int.Parse(inmatat);
+            Console.WriteLine("Mata in stop: ");
+            int stop = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mata in steg: ");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = start; i<=stop; i= i + steg) { Console.WriteLine(i); }
 
         }
     }
