19708e44e1510866bfd6d75ee0a8ba2b541d5efe
antonnn1 <antonnn@vgy.se>
2016-10-08 18:28:49 +0200

    ovn2.5

--- forLoopar/Program.cs
+++ forLoopar/Program.cs
@@ -38,6 +38,7 @@ namespace forLoopar
             for (int i = start; i<=stop; i= i + steg) { Console.WriteLine(i); }
             */
 
+            /*
             //Ovn 2.3
             Console.Write("Mata in ett heltal: ");
             int heltal = int.Parse(Console.ReadLine());
@@ -45,8 +46,46 @@ namespace forLoopar
             for (int i = 0; i<=heltal; i++)
             {summa = summa + i;}
             Console.WriteLine(summa);
+            */
 
+            /*
+            //Ovn 2.4
+            Console.Write("Mata in ett heltal: ");
+            int heltal1 = int.Parse(Console.ReadLine());
 
-      }
+            for (int i = 1; i <= heltal1; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                { Console.WriteLine("Talet " + i + " är jämt delbart med både 3 och 7"); }
+            }
+            if (heltal1 % 3 != 0 || heltal1 % 7 != 0)
+            {
+                Console.WriteLine("Inga tal hittades...");
+            }
+            */
+            //Övn 2.5
+            Console.WriteLine("Mata in ett heltal: ");
+            int inmatat = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 1;
+            int n;
+            if (inmatat == 0)
+            {
+                Console.WriteLine("Fibonaccitalen blir: 0");
+            }
+            else
+            {
+               Console.WriteLine("Fibonaccitalen blir: 1");
+               for (int i = 0; i < (inmatat-1); i++)
+               {
+                  n = x + y;
+                  x = y;
+                  y = n;
+                  Console.WriteLine(n + "  ");
+               }
+
+             }
+
+        }
     }
 }
